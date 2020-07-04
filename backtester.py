1e52be00877042487e0364ea98ae98aa8eee6be0
Houssam Aït <houssam.ouaziz@gmail.com>
2020-07-04 14:47:35 +0100

    Create backtester.py

---
+++ backtester.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import requests, json
+
+
+def start():
+    
+    print( "Starting Crypto Backtester V1" )
+    ticker = input("Enter ticker: ").upper()
+    data_url = 'https://min-api.cryptocompare.com/data/histominute?fsym=' + ticker + '&tsym=USD&limit=2000&aggregate=1'
+    response = requests.get(data_url)
+    try:
+        data = response.json()['Data']
+    except:
+        print( "Sorry, this crypto isn't supported!")
+        quit()
+    historical_data = data
+    print ("Fetched historical data for crypto: " + ticker)
+    cash = input("Enter initial investment: ")
+    strategy = input("Select (1) for the moving averages strategy: ")
+    if strategy == "1":
+        moving_averages(historical_data, ticker, cash)
+        
+
+
