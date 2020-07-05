c20d6c03b927ef6af1522a9b0d07cba182dbb654
Houssam Aït <houssam.ouaziz@gmail.com>
2020-07-05 11:51:48 +0100

    Update backtester.py
    for now let's just test the moving averages strategy, defining the corresponding function
    intial investement stored in "initial"  as an integer as well as cash variable (casting)
    crypto <- 0
    x and y empty arrays
    we'll work with a 3 & 5 day averages
    so when we loop through the historical data, we should start from the 5th index to even things out with the averages
    we will calculate the 3 & 5 averages by passing the data points ( as an array) through the get_average function that we'll define later
    the theory behind the moving averages strategy is obvious, no need to further explain it( the comparisons and when to buy or sell and where the portfolio is currently..)
    the actual profit/loss of the portfolio will be plotted by the matplotlib function

--- backtester.py
+++ backtester.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import requests, json 
 
 """
-     we need inputs to the ticker ( to fetch the historical data ), initial investement and the strategy the user wants to test 
+     we need inputs for the ticker ( to fetch the historical data ), initial investement and the strategy the user wants to test 
     
     """
 def start():
@@ -23,3 +23,55 @@ def start():
     if strategy == "1":
         moving_averages(historical_data, ticker, cash)
 
+
+def moving_averages(historical_data, ticker, cash):
+    """
+    If the 3 day average price of ETH is above the 5 day average price, buy. If below, sell.
+    """
+    initial = int(cash)
+    cash = int(cash)
+    crypto = 0
+    x_values = []
+    y_values = []
+    for place, data_set in enumerate(historical_data[5:-1]):
+        three_day_average = get_average([historical_data[place-1], historical_data[place-2], historical_data[place-3]])
+        five_day_average = get_average([historical_data[place-1],
+                                       historical_data[place-2],
+                                       historical_data[place-3],
+                                       historical_data[place-4],
+                                       historical_data[place-5]])
+        if three_day_average > five_day_average:
+            cash_used_to_buy = cash/2
+            price = float(data_set["close"])
+            number_of_crypto_we_just_bought = cash_used_to_buy/price
+            crypto += number_of_crypto_we_just_bought
+            cash -= cash_used_to_buy
+            print( "Just bought: " + str(number_of_crypto_we_just_bought) + " " + ticker ) 
+        if crypto > 1 and three_day_average < five_day_average:
+            price = float(data_set["close"])
+            number_of_crypto_being_sold = crypto/2
+            new_cash = number_of_crypto_being_sold * price
+            cash += new_cash
+            crypto -= number_of_crypto_being_sold
+            print( "Just sold: " + str(number_of_crypto_being_sold) + " " + ticker )
+        portfolio_value = cash + (crypto * float(data_set["close"]))
+        x_values.append(place)
+        y_values.append(portfolio_value)
+    print( "Final portfolio value: " + str(portfolio_value) )
+    net = portfolio_value - initial
+    if (net > 0):
+        print( "You profited: " + str(net)  )
+    else:
+        print("You lost:  " + str(net)   )
+    plot_graph(x_values, y_values)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
